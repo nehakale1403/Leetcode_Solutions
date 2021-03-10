fec8253ca00536ab2ab50a93e48df000e1503fb9
Neha Kale <nkale8013@gmail.com>
2021-03-10 23:29:45 +0530

    Adding two number and storing in lniked list

---
+++ Practice_problems/add_two_numbers.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <string>
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *result = new ListNode();
+        ListNode *curr = result;
+        int carry=0;
+        while(l1||l2){
+            int num1 = l1 ? l1->val: 0;
+            int num2=l2 ? l2->val :0;
+            int sum = num1 + num2+carry;
+            carry=sum/10;
+            curr->next=new ListNode(sum%10);
+            curr=curr->next;
+            if(l1)
+                l1=l1->next;
+            if(l2)
+                l2=l2->next;
+            
+        }
+        if(carry){
+            curr->next = new ListNode(carry);
+        }
+        return result->next;
+    }
+};
